1ef384ccd924d42a613a73d61b7858fc5a378921
Kristofer Krause <kris.krause@gmail.com>
2014-01-01 22:04:09 +0000

    default sound tap

--- src/com/kriskrause/learning/MainActivity.java
+++ src/com/kriskrause/learning/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.content.Context;
 import android.content.Intent;
+import android.view.SoundEffectConstants;
 
 public class MainActivity extends Activity implements OnClickListener, ICallbackListener
 {
@@ -68,6 +69,8 @@ public class MainActivity extends Activity implements OnClickListener, ICallback
 
     @Override
     public void onClick(View v) {
+	v.playSoundEffect(SoundEffectConstants.CLICK);
+
 	UpdateCharTask taskGetChar = new UpdateCharTask();
 
 	taskGetChar.setCallback(this);
