2c079b4c709a93bc421d3f0dd7734691f1b26ce0
Kristofer Krause <kris.krause@gmail.com>
2014-10-15 06:40:20 -0400

    help activity

--- app/src/main/java/com/kriskrause/learning/HelpActivity.java
+++ app/src/main/java/com/kriskrause/learning/HelpActivity.java
@@ -2,7 +2,10 @@ package com.kriskrause.learning;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.style.UnderlineSpan;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 public class HelpActivity extends Activity {
 
@@ -13,6 +16,18 @@ public class HelpActivity extends Activity {
         setContentView(R.layout.activity_help);
 
         getActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView txtMainTitle = (TextView) findViewById(R.id.titleMain);
+        TextView txtMainReviews = (TextView) findViewById(R.id.titleReviews);
+
+        SpannableString spanStringMain = new SpannableString(getResources().getString(R.string.help_mainTitle));
+        spanStringMain.setSpan(new UnderlineSpan(), 0, spanStringMain.length(), 0);
+
+        SpannableString spanStringReviews = new SpannableString(getResources().getString(R.string.help_reviewTitle));
+        spanStringReviews.setSpan(new UnderlineSpan(), 0, spanStringReviews.length(), 0);
+
+        txtMainTitle.setText(spanStringMain);
+        txtMainReviews.setText(spanStringReviews);
     }
 
     @Override
